16b3e83f3da3b55e3823d841e8e8b1b47c35572e
jiyeon2411 <daz128@naver.com>
2023-07-06 17:35:29 +0900

    spring 쇼핑몰

--- shopmax/src/main/java/com/shopmax/repository/ItemImgRepository.java
+++ shopmax/src/main/java/com/shopmax/repository/ItemImgRepository.java
@@ -1,9 +1,12 @@
 package com.shopmax.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopmax.entity.ItemImg;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
-
+	//select * from item_img where item_id = ?)
+	List<ItemImg> findByItemIdOrderByIdAsc(Long itemid);
 }
