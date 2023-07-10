7705491e6af17176a09b097a624dd9e2487f0458
jiyeon2411 <daz128@naver.com>
2023-07-10 18:01:17 +0900

    up

--- shopmax/src/main/java/com/shopmax/repository/ItemImgRepository.java
+++ shopmax/src/main/java/com/shopmax/repository/ItemImgRepository.java
@@ -9,4 +9,7 @@ import com.shopmax.entity.ItemImg;
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 	//select * from item_img where item_id = ?)
 	List<ItemImg> findByItemIdOrderByIdAsc(Long itemid);
+	
+	//select * from item_img where item_id = ? and repimg_yn = ?
+	ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
 }
